bf2e2c3d2ce2b8919a17b696916a19d0fd1cf64d
Cristian Aranda <cristianzozimo.aranda@gmail.com>
2018-11-05 17:03:12 -0300

    subo lo ultimo hecho para bluetooth

--- CodigoBluetooth/receptorBluetooth.asm
+++ CodigoBluetooth/receptorBluetooth.asm
@@ -1,7 +1,7 @@
 ;------------------------------------------------------------------------------------
-;Titulo: 			Configuración modulo Bluetooth
+;Titulo: 			EncendiendoLedconBluetooth
 ;Fecha de Creación: 4/10/2018
-;Autor: 			Cristian Z. Aranda C.
+;Autor: 			Cristian Z. Aranda 
 ;Editado por:
 ;
 ;
@@ -15,44 +15,24 @@
 
 
 ; Las siguientes constantes establecen el UBRR0L y UBRRH para definir el BAUDE RATE
-.EQU	constL=0x19		;baudaje de 38400
+.EQU	constL=0x67		;baudaje de 9600
 .EQU	constH=0x00
-;Constantes para la configuracion del modulo bluetooth
-
-.EQU	\r=0x0D					;\r y \n, no modificar!!!!
-.EQU	\n=0x0A					;Necesarios al final de cada comando AT
-.EQU	role='0'				;constante para definir el rol del bluetooth
-								;role=0-->Slave, role=1-->Master, Modificar según necesidad	
-.EQU	stop='0'				;stop=0-->1 bit de stop, stop=1-->2 bit de stop
-.EQU	parity='0'				;parity=0--> sin paridad, parity=1-->paridad impar, parity=2-->paridad par
-
 
 
 .DEF	buffer=R5				;exclusivo para enviar los datos al udreo
 .DEF	var1=R16
 
-.cseg 
-;----------Comandos AT--------------
-.org 0x500
-
-	;AT:		.dB	"AT",\r,\n			;Solicitud de configuracion Comandos AT,\r y \n, Necesarios al final de cada comando AT
-	AT:		.dB	"AT",\r,\n	
-	ATPSWD:	.dB	"AT+PSWD=9564",\r,\n	;Modificar <1234> para cambiar el password del modulo
-	;AT:		.dB	"AT",\r,\n
-	ATROLE:	.dB	"AT+ROLE=",role,\r,\n	;Modificar <role>  en la Parte de EQU=, para cambiar el nombre rol del bluetooth
-	;AT:		.dB	"AT",\r,\n
-	ATUART:	.dB	"AT+UART=9600",stop,parity,\r,\n	;Modificar stop y parity en EQU, Cambiar <9600> para el baurate									
-	ATNAME:	.dB	"AT+NAME=cris",\r,\n,0xFF	;Modificar <prueba> para cambiar el nombre del bluetooth
 
 
 
-
+.cseg
 .org 0x0000
 
 		rjmp 	main
 
-.org UDREaddr		
-		rjmp UDRE_INT_HANDLER
+
+.org URXCaddr	
+		rjmp URXC_INT_HANDLER
 
 
 ;.org INT0addr
@@ -73,20 +53,17 @@ main:
 		out SPL,var1
 		ldi var1, HIGH(RAMEND)
 		out SPH,var1
-
-		ldi ZH,HIGH(AT<<1)
-		ldi ZL,LOW(AT<<1)		
-
 		call Set_ports
 		call set_usart
+		call prender_bluetooth
 		sei							;habilito interrupcion global
 		
 
 again:		
-		cbi portb,5
-		call delay_500ms
-		sbi portb,5
-		call delay_500ms
+		;cbi portb,5
+		;call delay_500ms
+		;sbi portb,5
+		;call delay_500ms
 		rjmp again
 
 
@@ -115,7 +92,7 @@ again:
 ;Tamaño de caracter(frame)= 8bits	(en UCSR0C/B-->UCSZ0,UCSZ1/UCSZ2)
 ;Sin paridad						(en UCSR0C-->UMSEL0,UMSEL1)
 ;1 bit de stop 						(en UCSR0C-->USBS0)
-;Establecer un Bauderate de 38400	(en UBRR0H/UBRR0L )
+;Establecer un Bauderate de 9600	(en UBRR0H/UBRR0L )
 ;con un micro de 16MHZ(Ver formula)
 
 
@@ -126,14 +103,14 @@ set_usart:
 				;ldi r16,(1<<UDRE0)
 				;STS UCSR0A,r16
 				
-				ldi r16, (0<<RXEN0)|(1<<TXEN0)|(1<<UDRIE0)|(0<<TXCIE0); agrego interrupcion de transmision completa
+				ldi r16, (1<<RXEN0)|(1<<TXEN0)|(0<<UDRIE0)|(0<<TXCIE0)|(1<<RXCIE0); agrego interrupcion de transmision completa
 				STS UCSR0B,r16; No se puede usar OUT pues UCSR0B esta en la memoria extendida.
 
 				;Tamaño de caracter(frame)= 8bits + 1 bit de stop
 				ldi r16, (1<<UCSZ00)|(1<<UCSZ01)|(0<<USBS0)
 				STS UCSR0C,r16
 
-				;Establecer un Bauderate de 38400 con clock 16MHZ
+				;Establecer un Bauderate de 9600 con clock 16MHZ
 				ldi r16, constL
 				STS	UBRR0L,r16
 				ldi r16, constH
@@ -175,74 +152,6 @@ Set_ports:		sbi ddrb,0		;PWR para modulo(digital pin 8)
 				;out ddrc,r16	;Puerto C como entrada
 				ret 
 
-;------------------------------------------------------------------------------------
-
-;En vez de hacer polling de RXC0 para ver si esta en alto uso la interrupcion 
-
-UDRE_INT_HANDLER:	
-					push r16
-					push r17
-					push r18
-					push r19
-					push r20
-					;ponemos un delay de 4 seg para entrar en modo comandos
-					;sbi portb,5
-					
-					call delay_1seg
-					call delay_1seg
-					;cbi portb,5
-					;-------------------------------------------------------
-					;prender bluetooth ver como prender una sola vez
-					call prender_bluetooth
-					
-
-					;call delay_1seg
-					;call delay_1seg
-					;call delay_1seg
-					;call delay_1seg
-					;--------------------------------------------------------
-					ldi r16,0xFF
-					
-		send_AT:	lpm buffer,Z+		;cargo los comandos AT 
-					cp buffer,r16
-					brne send
-					;desactivo la interrupcion para que que no envie el mjs indefinidamente
-					ldi r16, (1<<RXEN0)|(1<<TXEN0)|(0<<UDRIE0)|(0<<TXCIE0); agrego interrupcion de transmision completa
-					STS UCSR0B,r16
-					pop r20
-					pop	r19
-					pop	r18
-					pop	r17
-					pop	r16
-					;sbi portb,5
-					;call delay_1seg
-					;call delay_1seg
-					;call delay_1seg
-					;call delay_1seg
-					;cbi portb,5
-					;call delay_1seg
-					;call delay_1seg
-					;call delay_1seg
-					;call delay_1seg
-					
-					reti
-		
-		send:		
-					sts UDR0,buffer
-					
-					;sbi portb,5
-					;call delay_500ms
-					;call delay_1seg
-					;cbi portb,5
-					pop r20
-					pop	r19
-					pop	r18
-					pop	r17
-					pop	r16
-					reti
-
-;------------------------------------------------------------------------------------
-
 prender_bluetooth:
 				
 				in r16,portb
@@ -250,4 +159,39 @@ prender_bluetooth:
 				ret
 				sbi portb,0
 				ret
+;------------------------------------------------------------------------------------
+
+
+;-------------------------------INTERRUCIONES----------------------------------------
+
+
+URXC_INT_HANDLER:	push r16
+					push r17
+					push r18
+					push r19
+					push r20
+					lds r17,UDR0	; cargo el mensaje 		
+					cpi r17,'c'		; Si recibo 'c'
+					brne salir		;salgo si no es 'c'
+					sbi portb,5
+					call delay_500ms
+					;call delay_1seg
+					;call delay_1seg
+					cbi portb,5
+					pop r20
+					pop	r19
+					pop	r18
+					pop	r17
+					pop	r16
+					reti
+
+		salir:		pop r20
+					pop	r19
+					pop	r18
+					pop	r17
+					pop	r16
+					reti
+;------------------------------------------------------------------------------------
+
+
 
