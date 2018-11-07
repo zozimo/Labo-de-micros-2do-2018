777747c2c87fd42c56e8ba54460b17a30fa8952c
Daniela Carrero <maria.daniela.carrero@ibm.com>
2018-11-07 09:37:55 -0300

    Actualizacion de periodo

--- Codigo Periodo/medir_periodo2.asm
+++ Codigo Periodo/medir_periodo2.asm
@@ -3,11 +3,22 @@
 .cseg 
 .org 0x00
 	JMP MAIN
-.org 0x02
-	JMP INTERRUPT
+;.org ICP1addr
+;	JMP INTERRUPT
 
 
 	MAIN: 
+		CBI DDRB, 0
+		SBI DDRB, 5
+		SBI DDRB, 6
+
+		;LDI R20, (1<<INT0)
+		;out EIMSK, R20;set INT0
+			
+		;LDS R20, EICRA ;rising edge INT0
+		;ORI R20, 0x03
+		;STS EICRA, R20
+
 		LDS R20, TIMSK1
 		ORI R20, 0x20
 		STS TIMSK1, R20 
@@ -19,28 +30,36 @@
 		LDS R20, TCCR1B
 		ORI R20, 0x41
 		STS TCCR1B,R20 ; rising edge, no prescaler, no noise canceller
-		SEI
+		;SEI
 	
+	HERE: CALL MEASURE_PERIOD
+		   JMP HERE
 	
-	INTERRUPT:
+	MEASURE_PERIOD:
+		;SBI PORTB, 5
+	;	CBI PINB, 0
+	;	SEI
 	L1:
-		LDS R21, TIFR1 ;timer interrupt
+		IN R21, TIFR1 ;timer interrupt
 		;When there's an interruption, ICF1 flag is set.
 		SBRS R21, ICF1 ; Skip next if ICF1 flag is set.
 		RJMP L1; loop until there's an interruption (?
 		LDS R23, ICR1L
 		LDS R24, ICR1H
-		STS TIFR1, R21 ; clear ICF1
+		OUT TIFR1, R21 ; clear ICF1
+		SBI PORTB, 5
 
 	L2:
-		LDS R21, TIFR1
+		IN R21, TIFR1
 		SBRS R21, ICF1 ; Skip next if ICF1 = 1
 		RJMP L2
-		STS TIFR1, R21 ; clear ICF1
+		SBI PORTB, 6
+		OUT TIFR1, R21 ; clear ICF1
 		LDS R22, ICR1L
 		SUB R22, R23; Period = Second edge - First edge
-		OUT PORTC, R22 ; REVISAR
-		LDS R22, ICR1H
-		SBC R22, R24; R22 = R22 - R24 - C
-		OUT PORTB, R22 ; REVISAR
-	RETI
+		;OUT PORTC, R22 ; REVISAR
+		LDS R23, ICR1H
+		SBC R23, R24; R23 = R23 - R24 - C
+		CBI PORTB, 5
+		;OUT PORTB, R22 ; REVISAR
+	RET
