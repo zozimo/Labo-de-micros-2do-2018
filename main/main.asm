064f220fc3d9900d6067fb27caa278c5b8231785
Pablo-J-Gonzalez <davincirobotico08@gmail.com>
2018-11-27 12:33:56 -0300

    Cambios generales menores
    Se cambio el orden de validación de velocidad OK: primero por velocidad baja y luego por velocidad alta
    Se agregaron comentarios en general
    Se dejan comentados posibles cambios (uso de prescaler en config_timer, display de varios mensajes en una misma vuelta, validaciones con LEDs de partes individuales, entre otros)

--- main/main.asm
+++ main/main.asm
@@ -4,7 +4,6 @@
 .device atmega328p
 
 .def input_reg = R16 ;
-.def output_reg = R17 ;
 
 .equ msg_size = 60	; mensaje de 10 símbolos ascii (6 bytes cada uno)
 
@@ -16,13 +15,12 @@
 .EQU	\r=0x0D					;\r y \n, no modificar!!!!
 .EQU	\n=0x0A					
 .DEF	buffer=R5				;exclusivo para enviar los datos al udreo
-.DEF	timeForOneGrade=R23
+.DEF	timeForOneGrade_reg=R23
 
 ;---------	Reserva de memoria en RAM	------------
 .dseg
 .org SRAM_START
 	msg : .byte msg_size
-
 
 
 ;---------	Configuración de interrupciones	---------
@@ -106,41 +104,43 @@ DICCIONARIO:
 
 main:
 	;inicializa el SP
-	LDI output_reg, HIGH(RAMEND) ; Carga el SPH
-	OUT SPH, output_reg
-	LDI output_reg, LOW(RAMEND) ;Carga el SPL
-	OUT SPL, output_reg
+	LDI R17, HIGH(RAMEND) ; Carga el SPH
+	OUT SPH, R17
+	LDI R17, LOW(RAMEND) ;Carga el SPL
+	OUT SPL, R17
 
-	LDI output_reg, 0x0F
-	OUT DDRC,output_reg	; C0...C3 como salidas
-	SWAP output_reg
-	OUT DDRD,output_reg	; D4...D7 como salidas
+	LDI R17, 0x0F
+	OUT DDRC,R17	; C0...C3 como salidas
+	SWAP R17
+	OUT DDRD,R17	; D4...D7 como salidas
 
-	CALL CONFIG_TIMER
+	CALL CONFIG_TIMER	; configuracion de timer
 
-	CALL BLUETOOTH_TO_RAM
+	CALL BLUETOOTH_TO_RAM	; configuracion de modulo bluetooth
 	SEI ; interrupcion habilitada para recibir mensaje en RAM
 
-	CALL ST_MSG_TO_RAM
-here:
+	CALL ST_MSG_TO_RAM	; carga de mensaje preconfigurado
+
+DISPLAY_LOOP:
 	CALL PRINT_MSG
 	call delay_45_grades
 ;	CALL PRINT_MSG
-	JMP here
+	JMP DISPLAY_LOOP
 ;	JMP end
 
 ;------------------------------------------------
 ; recibe en Z la posición de la columna a imprimir
 ; avanza a la siguiente columna
+
 PRINT_COL:
 	PUSH R20
 	LPM R20,Z+
 	PUSH R20	; guardo el valor de r18 en el stack
 	ANDI R20,0x0F
-	OUT PORTC,R20	;C0 ... C3 con nibble inferior
+	OUT PORTC,R20	; C0 ... C3 con nibble inferior
 	POP R20		; recupero el valor de r18 del stack
 	ANDI R20, 0xF0
-	OUT PORTD, R20	;D4 ... D7 con nibble superior
+	OUT PORTD, R20	; D4 ... D7 con nibble superior
 	POP R20
 	RET
 
@@ -149,6 +149,7 @@ PRINT_COL:
 ;utiliza puntero z como intermedio
 ;input_reg esta usado como ascii
 ;posición de la letra = DICCIONARIO + (input_reg -' ')*6
+
 PRINT_LETTER:
 	PUSH input_reg
 	PUSH R18
@@ -179,17 +180,19 @@ PRINT_LETTER:
 	RET
 
 ;------------------------------------------------
-;lee de RAM una frase e imprime letra por letra
-;usa input_reg para cada letra individual
+;	lee de RAM una frase e imprime letra por letra
+;	usa input_reg para cada letra individual
+
 PRINT_MSG:
 	LDI XH, HIGH(msg)	; variable para desplazarse en el mensaje en RAM
 	LDI XL, LOW(msg)
 	PRINT_MSG_LOOP:
 		LD input_reg, X+	; leer letra de RAM
-		CPI input_reg,0x00
+		CPI input_reg,0x00	; validacion contra fin de cadena
+		BREQ PRINT_MSG_END
+		CPI input_reg,0xFF	; validacion contra fin de cadena
 		BREQ PRINT_MSG_END
 		CALL PRINT_LETTER	; imprimir letra
-		;CALL DELAY_3_MS	; esperar letter_space
 		JMP PRINT_MSG_LOOP
 
 
@@ -219,7 +222,9 @@ ST_MSG_TO_RAM:
 ;------------------------------------------------
 ;	DELAYS
 ;------------------------------------------------
-;Delay de 1 grado que va a ser utilizado para generar delays de cierta cantidad de grados
+;	Delay de 1 grado que va a ser utilizado para generar delays de cierta cantidad de grados
+;	Usado como delay entre columnas del display
+
 DELAY_1_GRADE:
 		PUSH R20
 
@@ -234,26 +239,10 @@ DELAY_1_GRADE:
 
 		POP R20
 	RET
-;--------------------------------------------------
-;espera el tiempo correspondiente al espacio entre letras
-; 2.9ms a 16 MHz
-;DELAY_3_MS:
-;		push r22
-;		push R24
 
-;	    ldi  r22, 63
-;	    ldi  R24, 83
-;	LOOP_LETTER_SPACE: 
-;		dec  R24
-;	    brne LOOP_LETTER_SPACE
-;	    dec  r22
-;	    brne LOOP_LETTER_SPACE
-	
-;		pop R24
-;		pop r22
-		
-;	RET
 ;-------------------------------------------------
+; Delay usado como verificacion visual de la validación de la velocidad
+
 DELAY_500_MS:
 		push r18
 		push r19
@@ -277,6 +266,7 @@ DELAY_500_MS:
 ;------------------------------------------------
 ;espera el tiempo correspondiente al espacio entre columnas de una letra
 ; 599,5 us a 16 MHz
+
 DELAY_DOT_SPACE:
 	    ldi  r22, 13
 	    ldi  r23, 116
@@ -289,6 +279,7 @@ DELAY_DOT_SPACE:
 	RET
 
 ;------------------------------------------------
+; Delay para que el mensaje se muestre a partir de cierto angulo fijo
 delay_45_grades:
 	ldi R20, 45
 	loop:
@@ -298,9 +289,13 @@ delay_45_grades:
 		BRNE LOOP
 
 	ret
+
+
 ;------------------------------------------------
 ;	RUTINAS DE BLUETOOTH
 ;------------------------------------------------
+;	Preconfigura el Bluetooth para que reciba los mensajes y los almacene en RAM
+
 BLUETOOTH_TO_RAM:
 	ldi XH,HIGH(msg)
 	ldi XL,LOW(msg)
@@ -320,6 +315,7 @@ BLUETOOTH_TO_RAM:
 ;Establecer un Bauderate de 9600	(en UBRR0H/UBRR0L )
 ;con un micro de 16MHZ(Ver formula)
 ;-----------------------------------------------------------
+
 set_usart:
 ;--->>> Habilitar  receptor RXEN0 y habilito la interrupcion de recepcion completa RXCIE0
 	
@@ -337,10 +333,14 @@ set_usart:
 	STS	UBRR0H,r16
 	ret
 
+;-------------------------------------------------
+
 Set_ports:		
 	sbi ddrb,0		;PWR para modulo(digital pin 8)
 	sbi ddrb,5		;para el led de prueba
 	ret 
+
+;-------------------------------------------------
 
 prender_bluetooth:
 	in r16,portb
@@ -348,6 +348,8 @@ prender_bluetooth:
 	ret
 	sbi portb,0
 	ret
+
+;-------------------------------------------------
 
 reset_RAM:
 	ldi XH,HIGH(msg)
@@ -373,16 +375,18 @@ CONFIG_TIMER:
 	STS TCCR1A, R20 ; Set timer as normal mode
 
 	LDS R20, TCCR1B
-	ORI R20, 0x41
+	ORI R20, 0x41	;b10000001
 	STS TCCR1B,R20 ; rising edge, no prescaler, no noise canceller
+	;ORI R20, 0x43	;b10000011
+	;STS TCCR1B,R20 ; rising edge, prescaler 64, no noise canceller
 
 	CALL MEASURE_PERIOD
 	
 	;config timer para el delay de 1 grado
 	LDI R24, 0xFF 
-	SUB R24, timeForOneGrade ;timeForOneGrade contiene el valor alto del tiempo del periodo. Este representaría el tiempo de un grado. Fue calculado en MEASURE_PERIOD
+	SUB R24, timeForOneGrade_reg ;timeForOneGrade_reg contiene el valor alto del tiempo del periodo. Este representaría el tiempo de un grado. Fue calculado en MEASURE_PERIOD
 	OUT TCNT0, R24
-	LDI R20, 0x01
+;	LDI R20, 0x01
 		
 	LDI R20, 0x00
 	OUT TCCR0A, R20
@@ -411,7 +415,7 @@ MEASURE_PERIOD:
 		;When there's an interruption, ICF1 flag is set.
 		SBRS R21, ICF1 ; Skip next if ICF1 flag is set.
 		RJMP MEASURE_PERIOD_L1; loop until there's an interruption (?
-		LDS timeForOneGrade, ICR1L
+		LDS timeForOneGrade_reg, ICR1L
 		LDS R24, ICR1H
 		OUT TIFR1, R21 ; clear ICF1
 		;SBI PORTB, 5  ;Solo para pruebas
@@ -420,13 +424,13 @@ MEASURE_PERIOD:
 		IN R21, TIFR1
 		SBRS R21, ICF1 ; Skip next if ICF1 = 1
 		RJMP MEASURE_PERIOD_L2
-		SBI PORTB, 6
+		;SBI PORTB, 6
 		OUT TIFR1, R21 ; clear ICF1
 		LDS R22, ICR1L
-		SUB R22, timeForOneGrade; Period = Second edge - First edge
+		SUB R22, timeForOneGrade_reg; Period = Second edge - First edge
 
-		LDS timeForOneGrade, ICR1H
-		SBC timeForOneGrade, R24; R23 = R23 - R24 - C
+		LDS timeForOneGrade_reg, ICR1H
+		SBC timeForOneGrade_reg, R24; R23 = R23 - R24 - C
 		;CBI PORTB, 5 ;Solo para pruebas
 	CALL SET_TIME_PER_GRADE
 	
@@ -436,23 +440,25 @@ MEASURE_PERIOD:
 	RET
 
 ;---------------------------------------------------
-SET_TIME_PER_GRADE:
-	CPI timeForOneGrade, maxSpeed ; Comparar con 0, quiere decir que la velocidad es demasiado alta
-	BREQ MEASURE_PERIOD
-	SBI PORTD, 7
-	CPI timeForOneGrade, minSpeed
-	LDI R20, minSpeed
-	CP R20, timeForOneGrade
-	BRSH MEASURE_PERIOD
-	SBI PORTC, 0
+;	Valida la velocidad dentro de un rango preconfigurado
+;	Si la velocidad es valida se ven por 500mseg los dos leds de los extremos prendidos 
 
+SET_TIME_PER_GRADE:
+	CPI timeForOneGrade_reg, minSpeed
+;	LDI R20, minSpeed
+;	CP R20, timeForOneGrade
+	BRSH MEASURE_PERIOD
+	SBI PORTC, 0	;se prende un led si la velocidad es mayor a la minima
+
+	CPI timeForOneGrade_reg, maxSpeed ; Comparar con 0, quiere decir que la velocidad es demasiado alta
+	BREQ MEASURE_PERIOD
+	SBI PORTD, 7	;se prende otro led si la velocidad es menor a la maxima
 
 	CALL DELAY_500_MS ; delay para hacer visible los LEDs
-	CBI PORTD, 7
+	CBI PORTD, 7	;se apagan ambos leds luego de ver que la velocidad es calibrada
 	CBI PORTC, 0
-
 	RET
-	
+
 
 ;-------------------------------
 ;	INTERRUPCIONES
@@ -476,14 +482,16 @@ URXC_INT_HANDLER:
 	pop	r16
 	reti
 
-END_BLUETOOTH_MSG:		
-	call reset_RAM
-	pop r20
-	pop	r19
-	pop	r18
-	pop	r17
-	pop	r16
-	reti
+	END_BLUETOOTH_MSG:		
+		call reset_RAM
+		pop r20
+		pop	r19
+		pop	r18
+		pop	r17
+		pop	r16
+		reti
+
+
 ;------------------------------
 ;	END
 end: jmp end
